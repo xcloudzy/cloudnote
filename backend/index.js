7a97d0024a79bb5bbf920fc3abd105f176cefb54
Amit Kumar <amit4.jammu@gmail.com>
2024-07-29 09:58:24 +0530

    deploy

--- backend/index.js
+++ backend/index.js
@@ -13,17 +13,18 @@ app.use(
     credentials: true,
   })
 );
-app.use(express.json());
-
-// Available Routes
-app.use("/api/auth", require("./routes/auth"));
-app.use("/api/notes", require("./routes/notes"));
 
 app.get("*", (req, res, next) => {
   res.status(200).json({
     message: "Connected to vercel app",
   });
 });
+
+app.use(express.json());
+
+// Available Routes
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/notes", require("./routes/notes"));
 
 app.listen(port, () => {
   console.log(`iNotebook backend is listening to http://localhost:${port}`);
